cfd27100be4418528b2173550a97e526a95b43e0
Vinay Maurya <mauryavinay008@gmail.com>
2021-10-19 12:23:04 +0530

    initial commit

---
+++ client/src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span className="text-muted">@2021 Vinay Maurya</span>
+      </div>
+    </footer>
+  );
+}
